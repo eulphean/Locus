e8e351e821e1430d8e8deeafdacf063eafcc3e22
Amay Kataria <amkatari@microsoft.com>
2020-06-07 14:57:44 -0500

    Testing shader

--- src/main.js
+++ src/main.js
@@ -3,29 +3,18 @@ let eyelid;
 let upperLid; 
 
 function preload(){
-  theShader = loadShader('shader.vert', 'shader.frag'); 
 }
 
 // ------------------------------- Sketch Setup ------------------------------
 function setup() {
-  var canvas = document.createElement("canvas");
-  var sandbox = new GlslCanvas(canvas);   
-
-  // let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  // canvas.parent('container');
-  // canvas.style('z-index', 0); 
-  eyelid = new Eyelid(); 
-  noStroke();
+  var canvas = document.getElementsByClassName("glslCanvas");
+  var sandbox = new GlslCanvas(canvas[0]); 
+  console.log(sandbox);
 }
 
 // ------------------------------- Sketch Draw (loop) ------------------------
 function draw() {
-  shader(theShader); 
-  rect(0, 0, width,height);
-  theShader.setUniform('u_resolution', [width, height]);
-  // theShader.setUniform('u_time', millis()/1000.0);
-  theShader.setUniform('u_position', [0, 0]); 
-  // eyelid.draw(); 
+  sandbox.setUniform("u_position", 0.5, 0.5);
 }
 
 // Window resize. 
