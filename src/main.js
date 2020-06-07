d9016323978b9f3a7b6dd58ae0a1c1fbf406ae9d
Amay Kataria <amkatari@microsoft.com>
2020-06-07 17:06:09 -0500

    Finally I have a GLSL canvas setup that actually works

--- src/main.js
+++ src/main.js
@@ -1,31 +1,15 @@
-let theshader; 
 let eyelid; 
 let upperLid; 
 var sandbox; 
-
-function preload(){
-}
-
 // ------------------------------- Sketch Setup ------------------------------
 function setup() {
-//   var canvas = document.createElement("canvas");
-// var sandbox = new GlslCanvas(canvas);
-  var canvas = document.getElementsByClassName("glslCanvas");
-  console.log(windowWidth); 
-  console.log(windowHeight);
-  canvas[0].width = windowHeight;
-  canvas[0].height = windowWidth;
-  console.log(canvas[0])
-  sandbox = new GlslCanvas(canvas[0]); 
-  // console.log(sandbox);
+  var canvas = document.getElementById("glslCanvas");
+  canvas.width = windowWidth;
+  canvas.height = windowHeight; 
+  sandbox = new GlslCanvas(canvas); 
 }
 
 // ------------------------------- Sketch Draw (loop) ------------------------
 function draw() {
   sandbox.setUniform("u_position", windowWidth/2, windowHeight/2);
-}
-
-// Window resize. 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
 }
