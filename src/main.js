2909d922a8d2d0f2d845d644369ecf0618d4e86e
Amay Kataria <amkatari@microsoft.com>
2020-06-07 15:31:11 -0500

    Check this library

--- src/main.js
+++ src/main.js
@@ -1,20 +1,28 @@
 let theshader; 
 let eyelid; 
 let upperLid; 
+var sandbox; 
 
 function preload(){
 }
 
 // ------------------------------- Sketch Setup ------------------------------
 function setup() {
+//   var canvas = document.createElement("canvas");
+// var sandbox = new GlslCanvas(canvas);
   var canvas = document.getElementsByClassName("glslCanvas");
-  var sandbox = new GlslCanvas(canvas[0]); 
-  console.log(sandbox);
+  console.log(windowWidth); 
+  console.log(windowHeight);
+  canvas[0].width = windowHeight;
+  canvas[0].height = windowWidth;
+  console.log(canvas[0])
+  sandbox = new GlslCanvas(canvas[0]); 
+  // console.log(sandbox);
 }
 
 // ------------------------------- Sketch Draw (loop) ------------------------
 function draw() {
-  sandbox.setUniform("u_position", 0.5, 0.5);
+  sandbox.setUniform("u_position", windowWidth/2, windowHeight/2);
 }
 
 // Window resize. 
