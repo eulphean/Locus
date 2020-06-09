e9e516894b6620653bf565e1c6c2756842b5516d
Amay Kataria <amkatari@microsoft.com>
2020-06-09 01:14:41 -0500

    Check on mobile

--- src/main.js
+++ src/main.js
@@ -4,18 +4,17 @@ var canvasSize = [600, 400];
 let factor = canvasSize[0]/canvasSize[1];
 // ------------------------------- Sketch Setup ------------------------------
 function setup() {
-  createCanvas(canvasSize[0], canvasSize[1]);
   var canvas = document.getElementById("glslCanvas");
-  canvas.width = canvasSize[0];
-  canvas.height = canvasSize[1]; 
+  canvas.width = windowWidth;
+  canvas.height = windowHeight; 
   sandbox = new GlslCanvas(canvas); 
   eyelid = new Eyelid(); 
+  noCanvas();
 }
 
 // ------------------------------- Sketch Draw (loop) ------------------------
 function draw() {
-  background(150);
   // Update eyelid based on what it's doing. 
-  sandbox.setUniform("u_position", 300, 200);
+  sandbox.setUniform("u_position", windowWidth/2, windowHeight/2);
   noLoop();
 }
