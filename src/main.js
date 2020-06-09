dc6a1c37c05fde0f490aea06b8b47fab41703952
Amay Kataria <amkatari@microsoft.com>
2020-06-09 01:13:10 -0500

    Testing shader changes on mobile device

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 let eyelid; 
 var sandbox; 
+var canvasSize = [600, 400];
+let factor = canvasSize[0]/canvasSize[1];
 // ------------------------------- Sketch Setup ------------------------------
 function setup() {
+  createCanvas(canvasSize[0], canvasSize[1]);
   var canvas = document.getElementById("glslCanvas");
-  canvas.width = windowWidth;
-  canvas.height = windowHeight; 
+  canvas.width = canvasSize[0];
+  canvas.height = canvasSize[1]; 
   sandbox = new GlslCanvas(canvas); 
   eyelid = new Eyelid(); 
-  noCanvas();
 }
 
 // ------------------------------- Sketch Draw (loop) ------------------------
 function draw() {
+  background(150);
   // Update eyelid based on what it's doing. 
-  sandbox.setUniform("u_position", windowWidth/2, windowHeight/2);
+  sandbox.setUniform("u_position", 300, 200);
   noLoop();
 }
